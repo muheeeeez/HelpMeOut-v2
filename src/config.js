719c7b997412a6dc2c9129ba23394ac6143a0c7c
muheeeeez <abdulmuiz.olatunbosun@gmail.com>
2025-04-08 14:53:14 -0400

    fixed api key problem

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@
 export const config = {
   // AssemblyAI API key for video transcription
   // Sign up at https://www.assemblyai.com/ to get your API key
-  ASSEMBLY_API_KEY: import.meta.env.VITE_ASSEMBLY_API_KEY || '1bcd183ffc084379bf81a5ed0c65e5a5',
+  ASSEMBLY_API_KEY: import.meta.env.VITE_ASSEMBLY_API_KEY,
   
   // Application settings
   APP_NAME: 'HelpMeOut',
